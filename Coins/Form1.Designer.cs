24949ea494ee12469f71af7eca062bec444683ca
Philippe Andrei <philippeandreid@gmail.com>
2024-11-26 16:46:31 +0800

    initial force commit

---
+++ Coins/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Coins
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.totalValue = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(410, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(357, 330);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(65, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Get Value";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(199, 59);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(34, 13);
+            this.totalLabel.TabIndex = 5;
+            this.totalLabel.Text = "Total:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // totalValue
+            // 
+            this.totalValue.AutoSize = true;
+            this.totalValue.Location = new System.Drawing.Point(239, 59);
+            this.totalValue.Name = "totalValue";
+            this.totalValue.Size = new System.Drawing.Size(0, 13);
+            this.totalValue.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totalValue);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label totalValue;
+    }
+}
+
---
+++ Coins/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageProcess2;
+
+namespace Coins
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image, processed;
+        List<List<Point>> coins;
+        bool[,] visited_coins;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            processed = (Bitmap)image.Clone();
+            BitmapFilter.Threshold(ref processed, 200);
+            pictureBox1.Image = processed;
+
+            CountCoins(processed);
+        }
+
+        private void CountCoins(Bitmap mp)
+        {
+            coins = new List<List<Point>>();
+            visited_coins = new bool[processed.Width, processed.Height];
+
+            int coins_count = 0;
+            int coins_total = 0;
+
+            for (int x = 0; x < processed.Width; x++)
+            {
+                for (int y = 0; y < processed.Height; y++)
+                {
+                    Color pixel = processed.GetPixel(x, y);
+
+                    if (pixel.R == 0 && !visited_coins[x, y])
+                    {
+                        List<Point> coin;
+                        int size;
+
+                        (coin, size) = GetCoin(x, y);
+
+                        if (size < 20) continue;
+
+                        coins.Add(coin);
+                        coins_count++;
+                        int coin_worth = GetValue(size);
+                        coins_total += coin_worth;
+                    }
+                }
+            }
+
+            totalValue.Text = (coins_total / 100) + "." + (coins_total % 100);
+        }
+
+        private (List<Point>, int) GetCoin(int x, int y)
+        {
+            List<Point> points = new List<Point>();
+            Bitmap image = (Bitmap)processed.Clone();
+
+            int size = 0;
+
+            Queue<Point> q = new Queue<Point>();
+            q.Enqueue(new Point(x, y));
+
+            while (q.Count > 0)
+            {
+                Point curr = q.Dequeue();
+                points.Add(curr);
+                int px = curr.X;
+                int py = curr.Y;
+
+                if (visited_coins[px, py])
+                {
+                    continue;
+                }
+
+                size++;
+
+                visited_coins[px, py] = true;
+
+                Color pixel = image.GetPixel(px, py);
+
+                if (px - 1 >= 0 && pixel.R == 0 && !visited_coins[px - 1, py])
+                {
+                    q.Enqueue(new Point(px - 1, py));
+                }
+
+                if (px + 1 < image.Width && pixel.R == 0 && !visited_coins[px + 1, py])
+                {
+                    q.Enqueue(new Point(px + 1, py));
+                }
+
+                if (py - 1 >= 0 && pixel.R == 0 && !visited_coins[px, py - 1])
+                {
+                    q.Enqueue(new Point(px, py - 1));
+                }
+
+                if (py + 1 < image.Height && pixel.R == 0 && !visited_coins[px, py + 1])
+                {
+                    q.Enqueue(new Point(px, py + 1));
+                }
+            }
+
+            return (points, size);
+        }
+
+        private int GetValue(int size)
+        {
+            if (size > 8000)
+            {
+                return 500;
+            }
+
+            if (size > 6000)
+            {
+                return 100;
+            }
+
+            if (size > 4000)
+            {
+                return 25;
+            }
+
+            if (size > 3500)
+            {
+                return 10;
+            }
+            
+            return 5;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            image = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = image;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+    }
+}
